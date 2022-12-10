fe7727c21a018f5be6cbd77584b91bc02613031d
Erick Ramirez <erick@bitso.com>
2022-12-09 19:20:31 -0500

    Distintos objetos

--- example9/main.go
+++ example9/main.go
@@ -5,10 +5,12 @@ import (
 	"flag"
 	"fmt"
 	"path/filepath"
+	"time"
 
 	"github.com/crossplane-contrib/provider-aws/apis"
 	ec2 "github.com/crossplane-contrib/provider-aws/apis/ec2/v1beta1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -52,7 +54,7 @@ func main() {
 
 	// Now we can use the client to issue lists
 
-	objects := []interface{}{
+	objects := []client.ObjectList{
 		&ec2.VPCList{},
 		&ec2.SubnetList{},
 		&ec2.InternetGatewayList{},
@@ -61,8 +63,63 @@ func main() {
 	}
 
 	for _, obj := range objects {
-		x := obj.(client.ObjectList)
-		getObjectList(c, x)
+		verifyObjectList(c, obj)
+		//getObjectList(c, obj)
+	}
+
+	myvpcs := &ec2.VPCList{}
+	err = c.List(context.Background(), myvpcs)
+	if err != nil {
+		fmt.Printf("ERROR: %v\n", err)
+
+		panic(err)
+	}
+	myitems := myvpcs.GetItems()
+	for _, item := range myitems {
+		item.GetCondition("Ready")
+	}
+	for _, vpc := range myvpcs.Items {
+		err := wait.PollImmediate(3*time.Second, 10*time.Minute, func() (done bool, err error) {
+
+			for _, condition := range vpc.Status.Conditions {
+				if condition.Status != "True" {
+					return false, nil
+				}
+			}
+
+			return true, nil
+		})
+		if err != nil {
+			fmt.Printf("wait.PollImmediate error waiting for condition to be true: %v\n", err)
+
+			panic(err)
+		}
+	}
+
+	// Subnets
+	subnets := &ec2.SubnetList{}
+	err = c.List(context.Background(), subnets)
+	if err != nil {
+		fmt.Printf("ERROR: %v\n", err)
+
+		panic(err)
+	}
+	for _, subnet := range subnets.Items {
+		err := wait.PollImmediate(3*time.Second, 10*time.Minute, func() (done bool, err error) {
+
+			for _, condition := range subnet.Status.Conditions {
+				if condition.Status != "True" {
+					return false, nil
+				}
+			}
+
+			return true, nil
+		})
+		if err != nil {
+			fmt.Printf("wait.PollImmediate error waiting for condition to be true: %v\n", err)
+
+			panic(err)
+		}
 	}
 }
 
@@ -74,7 +131,34 @@ func getObjectList(client client.Client, list client.ObjectList) {
 
 		panic(err)
 	}
+
 	kind := list.GetObjectKind().GroupVersionKind().Kind
 	fmt.Printf("%s: %v\n", kind, list)
+
+}
+
+func verifyObjectList(client client.Client, list client.ObjectList) {
+	err := client.List(context.Background(), list)
+	if err != nil {
+		fmt.Printf("ERROR: %v\n", err)
+
+		panic(err)
+	}
+
+	//x := list.(*ec2.VPCList)
+	switch v := list.(type) {
+	case *ec2.VPCList:
+		fmt.Println("VPC: ")
+		for _, item := range v.Items {
+			fmt.Println(item)
+		}
+	case *ec2.SubnetList:
+		fmt.Println("Subnets: ")
+		for _, item := range v.Items {
+			fmt.Println(item)
+		}
+	default:
+		fmt.Println("not valid")
+	}
 
 }
